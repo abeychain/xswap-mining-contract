5f39a53cd3383dc62ccd84251f8cb4d689b4fd59
BiyunZeng <chhaozeng@163.com>
2021-09-08 11:42:46 +0800

    add add() funtion with validatePoolByPid

--- contract/contracts/xtLpReward.sol
+++ contract/contracts/xtLpReward.sol
@@ -133,7 +133,7 @@ contract XTPool is Ownable {
 
     // Add a new lp to the pool. Can only be called by the owner.
     // XXX DO NOT add the same LP token more than once. Rewards will be messed up if you do.
-    function add(uint256 _allocPoint, IERC20 _lpToken, bool _withUpdate) public onlyOwner {
+    function add(uint256 _allocPoint, IERC20 _lpToken, bool _withUpdate) public onlyOwner validatePoolByPid(_pid){
         require(address(_lpToken) != address(0), "_lpToken is the zero address");
         if (_withUpdate) {
             massUpdatePools();
